4902ca4769a8e326c747ed52a79113daa80122d6
dmitrijmahov <mahovd@gmail.com>
2016-04-24 21:23:03 +0300

    Started implementing two-pane mode activity_twopane - layout for two-pane mode was added add new method getLayoutId to SingleFrameActivity

--- app/src/main/java/com/mahovd/bignerdranch/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/mahovd/bignerdranch/criminalintent/SingleFragmentActivity.java
@@ -1,6 +1,7 @@
 package com.mahovd.bignerdranch.criminalintent;
 
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
@@ -18,7 +19,7 @@ public abstract class SingleFragmentActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment);
+        setContentView(getLayoutResId());
 
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
@@ -29,6 +30,14 @@ public abstract class SingleFragmentActivity extends AppCompatActivity{
                     add(R.id.fragment_container,fragment).
                     commit();
         }
+
+    }
+
+    //Returns the Id of the layout that the activity will inflate
+    @LayoutRes
+    protected int getLayoutResId() {
+
+        return R.layout.activity_twopane;
 
     }
 
